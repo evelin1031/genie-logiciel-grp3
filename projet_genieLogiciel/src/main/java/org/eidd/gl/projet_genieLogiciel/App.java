e9278d019a25aeb1308b83894a0c406c859a68e6
mxyssxm <meyssem.soussou@gmail.com>
2026-01-09 17:07:28 +0100

    Structure Maven

---
+++ projet_genieLogiciel/src/main/java/org/eidd/gl/projet_genieLogiciel/App.java
@@ -0,0 +1,10 @@
+package org.eidd.gl.projet_genieLogiciel;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

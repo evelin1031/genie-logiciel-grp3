ed71d66e86e5561d56db4e25ded4d1843f9f45fe
evelin1031 <lineve1031@gmail.com>
2026-03-12 12:40:20 +0100

    Configuration tests Maven et JaCoCo

---
+++ projet_genieLogiciel/src/test/java/org/eidd/gl/projet_genieLogiciel/ConfigurationTest.java
@@ -0,0 +1,13 @@
+package org.eidd.gl.projet_genieLogiciel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ConfigurationTest {
+
+    @Test
+    void shouldPassSimpleTest() {
+        assertTrue(true);
+    }
+}

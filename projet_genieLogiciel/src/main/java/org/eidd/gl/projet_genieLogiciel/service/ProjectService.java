29a019f49b5bac17d489072e1b033de366877340
HxgoL <legehugo@gmail.com>
2026-03-15 17:25:16 +0100

    Modifs ProjectRepository, creéations ProjectService, ProjectRepositoryJSON

---
+++ projet_genieLogiciel/src/main/java/org/eidd/gl/projet_genieLogiciel/service/ProjectService.java
@@ -0,0 +1,28 @@
+package org.eidd.gl.projet_genieLogiciel.service;
+import org.eidd.gl.projet_genieLogiciel.persistance.ProjectRepository;
+
+public class ProjectService {
+    private final ProjectRepository repository;
+
+    public ProjectService(ProjectRepository repository) {
+        this.repository = repository;
+    }
+
+    public void updateProjectDescription(int projectId, String newDescription) {
+        Project project = repository.findById(projectId);
+        if (project == null) {
+            throw new IllegalArgumentException("Projet introuvable");
+        }
+        project.setDescription(newDescription);
+        repository.save(project);
+    }
+
+    public void addTaskToProject(int projectId, Task task) {
+        Project project = repository.findById(projectId);
+        if (project == null) {
+            throw new IllegalArgumentException("Projet introuvable");
+        }
+        project.addTask(task);
+        repository.save(project);
+    }
+}

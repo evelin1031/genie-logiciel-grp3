779c0398180a03cb1cce19aeaf18b6ca49f356c4
mxyssxm <meyssem.soussou@gmail.com>
2026-03-19 11:15:19 +0100

    Add TaskService and TaskServiceTest

--- projet_genieLogiciel/src/main/java/org/eidd/gl/projet_genieLogiciel/service/TaskService.java
+++ projet_genieLogiciel/src/main/java/org/eidd/gl/projet_genieLogiciel/service/TaskService.java
@@ -1,5 +1,40 @@
 package org.eidd.gl.projet_genieLogiciel.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.eidd.gl.projet_genieLogiciel.metier.Task;
+import org.eidd.gl.projet_genieLogiciel.metier.TaskStatus;
+import org.eidd.gl.projet_genieLogiciel.persistance.TaskRepository;
+
 public class TaskService {
 
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public void createTask(Task task) {
+        taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(int id) {
+        return taskRepository.findById(id);
+    }
+
+    public void deleteTask(int id) {
+        taskRepository.delete(id);
+    }
+
+    public void updateTaskStatus(int id, TaskStatus status) {
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()) {
+            task.get().setStatus(status);
+        }
+    }
 }
